516e2258e10f0a0caa1a8d053980de7e2611287e
broken-helix <>
2023-05-24 16:29:47 +0000

    deployment commit

--- djangotesting/settings.py
+++ djangotesting/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,7 +29,7 @@ SECRET_KEY = 'SECRET_KEY = os.environ.get("SECRET_KEY")'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-broken-helix-django-test-e020ea2575.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['meal-planner-test.herokuapp.com', '8000-broken-helix-django-test-e020ea2575.us2.codeanyapp.com']
 
 
 # Application definition
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'djangotesting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
